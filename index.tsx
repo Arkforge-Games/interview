12600401fa97878b2f8feefad1bf10cb4c6c4bc4
tony <9378129+casterfile@users.noreply.github.com>
2026-02-09 12:29:59 +0800

    update Test

--- index.tsx
+++ index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const APP_VERSION = 'v0.0.1';
+console.log(`%cSlayJobs ${APP_VERSION}`, 'color: #7c3aed; font-weight: bold; font-size: 14px;');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
